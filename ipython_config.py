234a9c1135bc888c58f47ddf77d6d4a29246bf80
gagolews <m.gagolewski@gmail.com>
2024-08-23 11:32:10 +0200

    more

---
+++ ipython_config.py
@@ -0,0 +1,14 @@
+# Copyleft (C) 2024, Marek Gagolewski <https://www.gagolewski.com>
+# Configuration file for IPython
+
+# ~/.ipython/profile_default/ipython_config.py:
+#     c = get_config()
+#     exec(open('/home/gagolews/bin/ipython_config.py').read())
+
+#import numpy as np
+#import pandas as pd
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+#print("Loaded module aliases: np, pd, plt, sns.");
+
+c.TerminalIPythonApp.display_banner = False
