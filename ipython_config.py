95a6d5924e1b6f48599b2fd7cb881fd1c0d5710d
gagolews <m.gagolewski@gmail.com>
2024-12-17 10:49:43 +0100

    more

--- ipython_config.py
+++ ipython_config.py
@@ -1,14 +1,18 @@
 # Copyleft (C) 2024, Marek Gagolewski <https://www.gagolewski.com>
 # Configuration file for IPython
 
-# ~/.ipython/profile_default/ipython_config.py:
-#     c = get_config()
-#     exec(open('/home/gagolews/bin/ipython_config.py').read())
+# ln -s ~/bin/ipython_config.py ~/.ipython/profile_default/ipython_config.py
 
-#import numpy as np
-#import pandas as pd
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-#print("Loaded module aliases: np, pd, plt, sns.");
+c.TerminalIPythonApp.exec_lines = """
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+print("Loaded module aliases: np, pd, plt, sns.", end="")
+"""
 
 c.TerminalIPythonApp.display_banner = False
+c.TerminalIPythonApp.matplotlib = "qtcairo"
+#c.TerminalInteractiveShell.history_load_length = 0
+#c.TerminalInteractiveShell.history_length = 1000
+c.TerminalInteractiveShell.autosuggestions_provider = None
